fa638a82ddae75f41380ac6036870623ae5f8889
Karan Khatik <100562135+Karankhatik@users.noreply.github.com>
2024-08-26 00:04:58 +0530

    chnages to test cors on prod

--- api/index.js
+++ api/index.js
@@ -1,31 +1,33 @@
 const express = require('express');
-const app = express();
 const cors = require('cors');
+const app = express();
 require('dotenv').config();
 const connectDB = require('./config/database');
 
-
+// Connect to the database
 connectDB();
 
-app.use(express.json())
-app.options('*', cors()); // Enable preflight requests for all routes
+// Use JSON middleware
+app.use(express.json());
 
+// Set up CORS for all routes
+app.use(cors({
+    origin: ['https://fund-transfer-nine.vercel.app'], // Allow this frontend domain
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'], // Allowed methods
+    credentials: true
+}));
 
+// Enable preflight requests for all routes
+app.options('*', cors()); // This line
 
-
+// Define your routes
 app.use('/api/v1', require('./routes'));
 
-
-const PORT = process.env.PORT || 5000
-
+// Define the root route
 app.get('/', (req, res) => {
-    res.send('Hello World!')
+    res.send('Hello World!');
 });
 
-
-app.listen(PORT, () => console.log(`Server started on port ${PORT}\n`
-    + `http://localhost:${PORT}`
-))
-
-
-
+// Start the server
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server started on port ${PORT}\nhttp://localhost:${PORT}`));
